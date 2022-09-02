f1bb4187de9a7ba278099d90cf5b7951c55e520e
Ceowyllian <downwell@online.ua>
2022-09-02 14:01:00 +0300

    guess the number

--- proga2022/src/humans/App.java
+++ proga2022/src/humans/App.java
@@ -1,24 +1,16 @@
 package humans;
-import java.util.*;
 
+import java.util.LinkedList;
 
 public class App {
 
 	public static void main(String[] args) {
 		Address address = new Address("Ukraine", "Kyiv", "Streeeeeeet", 25);
-		Human user = new Human(
-					"John",
-					"Doe",
-					23,
-					175.23,
-					67.3,
-					true,
-					Nation.UKRANIAN,
-					address
-				);
+
+		Human user = new Human("John", "Doe", 23, 175.23, 67.3, true, Nation.UKRANIAN, address);
 		System.out.println(user);
 		System.out.print(address);
-		LinkedList<Human> people = new LinkedList<Human>();
+		LinkedList<Human> people = new LinkedList<>();
 		people.add(user);
-	}	
+	}
 }
--- proga2022/src/humans/Human.java
+++ proga2022/src/humans/Human.java
@@ -2,14 +2,17 @@ package humans;
 
 
 public class Human {
+	
     private String _name;
     private String _surname;
     private int _age;
     private double _height;
     private double _weight;
     private boolean _habbits;
+    
     private Nation _nation;
     private Address _adress;
+    
 	public Human(String _name, String _surname, int _age, double _height, double _weight, boolean _habbits,
 			Nation _nation, Address _adress) {
 		super();
@@ -22,54 +25,71 @@ public class Human {
 		this._nation = _nation;
 		this._adress = _adress;
 	}
+	
 	public String get_name() {
 		return _name;
 	}
+	
 	public void set_name(String _name) {
 		this._name = _name;
 	}
+	
 	public String get_surname() {
 		return _surname;
 	}
+	
 	public void set_surname(String _surname) {
 		this._surname = _surname;
 	}
+	
 	public int get_age() {
 		return _age;
 	}
+	
 	public void set_age(int _age) {
 		this._age = _age;
 	}
+	
 	public double get_height() {
 		return _height;
 	}
+	
 	public void set_height(double _height) {
 		this._height = _height;
 	}
+	
 	public double get_weight() {
 		return _weight;
 	}
+	
 	public void set_weight(double _weight) {
 		this._weight = _weight;
 	}
+	
 	public boolean is_habbits() {
 		return _habbits;
 	}
+	
 	public void set_habbits(boolean _habbits) {
 		this._habbits = _habbits;
 	}
+	
 	public Nation get_nation() {
 		return _nation;
 	}
+	
 	public void set_nation(Nation _nation) {
 		this._nation = _nation;
 	}
+	
 	public Address get_adress() {
 		return _adress;
 	}
+	
 	public void set_adress(Address _adress) {
 		this._adress = _adress;
 	}
+	
 	@Override
 	public String toString() {
 		return _name + " " + _surname + ", " + _age + " y. o.";
