69717d309d9a817b474c31b6cb18aae5022871b2
Ceowyllian <downwell@online.ua>
2022-09-02 21:11:11 +0300

    Address class

--- proga2022/src/humans/Address.java
+++ proga2022/src/humans/Address.java
@@ -1,69 +1,55 @@
 package humans;
 
 public class Address {
-	private String _country;
-	private String _city;
-	private String _street;
-	private int _house;
+    private String state;
+    private String city;
+    private String street;
+    private int house;
 
-	
-	public Address(String _country, String _city, String _street, int _house) {
-		super();
-		this._country = _country;
-		this._city = _city;
-		this._street = _street;
-		this._house = _house;
-	}
+    public Address(String state, String city, String street, int house) {
+        super();
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+    }
 
+    @Override
+    public String toString() {
+        return this.state + ", " + this.city + ", " + this.street + ", "
+                + this.house;
+    }
 
-	public String toString() {
-		return this._country + ", " 
-				+ this._city + ", " 
-				+ this._street + ", " 
-				+ this._house
-				+ Integer.toString(this._house);
-	}
+    public String get_country() {
+        return state;
+    }
 
+    public void set_country(String state) {
+        this.state = state;
+    }
 
-	public String get_country() {
-		return _country;
-	}
+    public String get_city() {
+        return city;
+    }
 
+    public void set_city(String city) {
+        this.city = city;
+    }
 
-	public void set_country(String _country) {
-		this._country = _country;
-	}
+    public String get_street() {
+        return street;
+    }
 
+    public void set_street(String street) {
+        this.street = street;
+    }
 
-	public String get_city() {
-		return _city;
-	}
+    public int get_house() {
+        return house;
+    }
 
-
-	public void set_city(String _city) {
-		this._city = _city;
-	}
-
-
-	public String get_street() {
-		return _street;
-	}
-
-
-	public void set_street(String _street) {
-		this._street = _street;
-	}
-
-
-	public int get_house() {
-		return _house;
-	}
-
-
-	public void set_house(int _house) {
-		this._house = _house;
-	}
-	
-	
+    public void set_house(int house) {
+        this.house = house;
+    }
 
 }
