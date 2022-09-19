f97019fb71f8e0381dcfecdcbf8b70f4ceedf975
Ceowyllian <downwell@online.ua>
2022-09-19 14:07:52 +0300

    Refactor

--- proga2022/src/humans/storage/PersonStorage.java
+++ proga2022/src/humans/storage/PersonStorage.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.UUID;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 import java.util.List;
 
 
@@ -13,8 +12,9 @@ import humans.models.Person;
 
 public class PersonStorage {
 
-    private PersonStorage() {}
-    
+    private PersonStorage() {
+    }
+
     private static HashSet<Person> storage;
 
     public static void initialize(int initialCapacity) {
@@ -23,51 +23,53 @@ public class PersonStorage {
     }
 
     private static boolean compare(
-            String operator, Comparable left, Comparable right) {
-        
+        String operator, Comparable left, Comparable right) {
+
         return switch (operator) {
             case "=" -> left.equals(right);
             case "!=" -> !left.equals(right);
             case ">" -> left.compareTo(right) > 0;
             case "<" -> left.compareTo(right) < 0;
             default -> throw new IllegalArgumentException(
-                    "Unexpected value: " + operator);
+                "Unexpected value: " + operator);
         };
     }
-    
+
     public static <T extends Comparable<T>> List<Person> filter(
-            String field, String operator, T constant) {
+        String field, String operator, T constant) {
 
         List<Person> result = new LinkedList<Person>();
-        Iterator<Person> querySet = storage
-                .stream()
-                .filter(switch(field) {
+        Predicate<Person> by_field = switch (field) {
             case "name" -> p -> compare(operator, p.getName(), constant);
             case "surname" -> p -> compare(operator, p.getSurname(), constant);
             case "age" -> p -> compare(operator, p.getAge(), constant);
             case "height" -> p -> compare(operator, p.getHeight(), constant);
             case "weight" -> p -> compare(operator, p.getWeight(), constant);
-            case "habits" -> p -> compare(operator, p.getAge(), constant);
+            case "habits" -> p -> compare(operator, p.hasHabits(), constant);
             default -> throw new IllegalArgumentException("No such field: " + field);
-        }).iterator();
-        querySet.forEachRemaining(result::add);
+        };
+        storage
+            .stream()
+            .filter(by_field)
+            .iterator()
+            .forEachRemaining(result::add);
         return result;
     }
-    
+
     public static Person find_by_id(UUID id) {
         Iterator<Person> querySet = storage
-                .stream()
-                .filter(p -> (p.Id() == id))
-                .iterator();
+            .stream()
+            .filter(p -> (p.Id() == id))
+            .iterator();
         if (querySet.hasNext())
             return querySet.next();
         return null;
     }
-    
+
     public static boolean create(Person person) {
         return storage.add(person);
     }
-    
+
     public static void create(List<Person> people) {
         storage.addAll(people);
     }
