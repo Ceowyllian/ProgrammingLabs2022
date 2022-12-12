f4604b4159bbdc8db663f0e6fc5ae182ec41d491
Ceowyllian <downwell@online.ua>
2022-12-12 13:12:19 +0200

    Rename App class

--- src/guess/App.java
+++ src/guess/App.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 
 public class App {
 
-	public static int random_int(int min, int max) {
-		return (int) ((Math.random() * (max - min)) + min);
-	}
+    public static int random_int(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
 
-	public static void main(String[] args) {
-		int number = random_int(0, 10);
-		int attempts = 3;
+    public static void main(String[] args) {
+        int number = random_int(0, 10);
+        int attempts = 3;
 
-		PrintStream console = System.out;
-		Scanner read = new Scanner(System.in);
+        PrintStream console = System.out;
+        Scanner read = new Scanner(System.in);
 
-		for (int i = 1; i <= attempts; i++) {
-			console.print("Enter number: ");
-			int input = read.nextInt();
-			if (input == number) {
-				console.println("\nYou guessed!");
-				break;
-			} else {
-				console.printf("\nWrong answer! Attempts left: %s%n", attempts - i);
-			}
-		}
-		console.printf("The number is: %s%n", number);
-	}
+        for (int i = 1; i <= attempts; i++) {
+            console.print("Enter number: ");
+            int input = read.nextInt();
+            if (input == number) {
+                console.println("\nYou guessed!");
+                break;
+            } else {
+                console.printf("\nWrong answer! Attempts left: %s%n", attempts - i);
+            }
+        }
+        console.printf("The number is: %s%n", number);
+    }
 }
