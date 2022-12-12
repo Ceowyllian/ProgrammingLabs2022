d562baa247ccb810bd4df7c2e1d3286c1fd19134
Ceowyllian <downwell@online.ua>
2022-12-12 13:17:09 +0200

    Update PersonStorage class

--- src/humans/storage/PersonStorage.java
+++ src/humans/storage/PersonStorage.java
@@ -1,77 +1,79 @@
 package humans.storage;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.UUID;
-import java.util.function.Predicate;
-import java.util.List;
-
-
 import humans.models.Person;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+
 public class PersonStorage {
+
+    private static HashSet<Person> storage;
 
     private PersonStorage() {
     }
 
-    private static HashSet<Person> storage;
-
     public static void initialize(int initialCapacity) {
-        if (storage == null)
-            storage = new HashSet<Person>(initialCapacity);
+        if (storage == null) storage = new HashSet<>(initialCapacity);
     }
 
-    private static boolean compare(
-        String operator, Comparable left, Comparable right) {
-
+    private static <T extends Comparable<V>, V extends Comparable<T>> boolean compare(
+        String operator, T left, V right
+    ) {
         return switch (operator) {
             case "=" -> left.equals(right);
             case "!=" -> !left.equals(right);
             case ">" -> left.compareTo(right) > 0;
             case "<" -> left.compareTo(right) < 0;
-            default -> throw new IllegalArgumentException(
-                "Unexpected value: " + operator);
+            default -> throw new IllegalArgumentException();
         };
+    }
+
+    private static <T extends Comparable<T>> T fieldValue(String field, Person p) {
+        return switch (field) {
+            case "name" -> (T) p.getName();
+            case "surname" -> (T) p.getSurname();
+            case "nation" -> (T) p.getNation();
+            case "age" -> (T) (Integer) p.getAge();
+            case "height" -> (T) (Double) p.getHeight();
+            case "weight" -> (T) (Double) p.getWeight();
+            default -> throw new IllegalArgumentException();
+        };
+    }
+
+    private static <T extends Comparable<T>> Predicate<Person> createPredicate(
+        String field, String operator, T constant
+    ) {
+        return p -> compare(operator, fieldValue(field, p), constant);
     }
 
     public static <T extends Comparable<T>> List<Person> filter(
-        String field, String operator, T constant) {
-
-        List<Person> result = new LinkedList<Person>();
-        Predicate<Person> by_field = switch (field) {
-            case "name" -> p -> compare(operator, p.getName(), constant);
-            case "surname" -> p -> compare(operator, p.getSurname(), constant);
-            case "age" -> p -> compare(operator, p.getAge(), constant);
-            case "height" -> p -> compare(operator, p.getHeight(), constant);
-            case "weight" -> p -> compare(operator, p.getWeight(), constant);
-            case "habits" -> p -> compare(operator, p.hasHabits(), constant);
-            default -> throw new IllegalArgumentException("No such field: " + field);
-        };
-        storage
-            .stream()
-            .filter(by_field)
-            .iterator()
-            .forEachRemaining(result::add);
+        String field, String operator, T constant
+    ) {
+        List<Person> result = new LinkedList<>();
+        Predicate<Person> rule = createPredicate(field, operator, constant);
+        storage.stream()
+               .filter(rule)
+               .iterator()
+               .forEachRemaining(result::add);
         return result;
     }
 
-    public static Person find_by_id(UUID id) {
-        Iterator<Person> querySet = storage
-            .stream()
-            .filter(p -> (p.Id() == id))
-            .iterator();
-        if (querySet.hasNext())
-            return querySet.next();
-        return null;
+    public static Person findById(UUID id) {
+        return storage.stream()
+                      .filter(p -> (p.id == id))
+                      .findFirst()
+                      .orElse(null);
     }
 
-    public static boolean create(Person person) {
-        return storage.add(person);
+    public static void create(Person person) {
+        storage.add(person);
     }
 
     public static void create(List<Person> people) {
         storage.addAll(people);
     }
-
 }
